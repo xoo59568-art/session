1385eeaf1bf054a0f0e4a7b9d100f7677bbc8f85
xoo59568-art <xoo59568@gmail.com>
2026-05-10 19:41:03 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -53,7 +53,7 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'DAVE-X:~' + b64data });
+                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'RABBITXMD:~' + b64data });
 
                     let Mbuvi_MD_TEXT = `
 ╔════════════════════◇
